869b6fb68aa6e0ee465562a4d84f6f53a1115bb1
HossamGezo <ha2ghossam10@gmail.com>
2025-11-02 19:55:36 +0200

    feat: Add banner section

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,7 @@
 import HeroSection from "./components/hero-section/HeroSection";
 import ServicesSection from "./components/services-section/ServicesSection";
 import ToursSection from "./components/tours-section/ToursSection";
+import Banner from "./components/banner/Banner";
 // Main Componet
 const Home = () => {
   return (
@@ -12,6 +13,8 @@ const Home = () => {
       <ServicesSection />
       {/* Tours Section */}
       <ToursSection />
+      {/* Banner Section */}
+      <Banner />
     </>
   );
 };
