3a9777279f91d3c3dd6536855473fa619b8145c5
HossamGezo <ha2ghossam10@gmail.com>
2025-11-04 18:25:33 +0200

    feat: Add about us page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,5 +1,15 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <div className="about container mx-auto pt-10 h-[calc(100vh-99.02px)]">
+        <h1 className="about-head text-5xl text-[#333] pb-3 border-b-2">About This App</h1>
+        <p className="about-desc text-xl my-5 tracking-wide">
+          This app is all about booking safari desert tour in Dubai
+        </p>
+        <p className="about-version"> <strong className="text-xl">Version</strong>: 1.0.0</p>
+      </div>
+    </>
+  );
 };
 
 export default About;
