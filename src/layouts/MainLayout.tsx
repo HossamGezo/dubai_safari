e957bd9754ac5ed0c3c83f38aba8835a2cd9a652
HossamGezo <ha2ghossam10@gmail.com>
2025-11-06 12:40:14 +0200

    fix: Scroll to top on route change

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,12 +1,14 @@
 // React Router Dom
-import { Outlet } from "react-router";
+import {Outlet} from "react-router";
 // Components
 import Footer from "../components/layout/Footer/Footer";
 import Header from "../components/layout/Header/Header";
+import ScrollToTop from "../components/common/scroll-to-top/ScrollToTop";
 // Main Components
 const MainLayout = () => {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
