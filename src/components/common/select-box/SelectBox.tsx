192fc93b3af865870dbd908e6a7c3fb637b829a7
HossamGezo <ha2ghossam10@gmail.com>
2026-02-19 19:01:22 +0200

    feat: enhance and expand reusable common components

--- src/components/common/select-box/SelectBox.tsx
+++ src/components/common/select-box/SelectBox.tsx
@@ -1,27 +1,30 @@
+// Utils
+import { cn } from "@/utils/cn";
+
 // Types
-type item = {
-  id: number;
-  value: string;
-};
+import type { OptionType } from "@/types/option.types";
+
+// Types
 type SelectBoxProps = {
-  items: item[];
-};
+  items: OptionType[];
+} & React.ComponentProps<"select">;
+
 // Main Component
-const SelectBox = ({items}: SelectBoxProps) => {
+const SelectBox = ({ items, className, ...rest }: SelectBoxProps) => {
   return (
-    <>
-      <select className="select-box bg-white text-[#333] py-2 mb-5 rounded-sm outline-0">
-        {items.map((item) => (
-          <option
-            key={item.id}
-            value={item.value}
-            className="select-box-item"
-          >
-            {item.value}
-          </option>
-        ))}
-      </select>
-    </>
+    <select
+      className={cn(
+        "select-box bg-white text-[#333] py-2 mb-5 rounded-sm outline-0",
+        className,
+      )}
+      {...rest}
+    >
+      {items.map((item) => (
+        <option key={item.id} value={item.value}>
+          {item.label || item.value}
+        </option>
+      ))}
+    </select>
   );
 };
 
